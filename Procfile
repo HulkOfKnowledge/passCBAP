5205b76c02831d07913cf151408bec5b0c038822
HulkOfKnowledge <ifebuemedaniel7@gmail.com>
2023-06-25 16:05:30 +0100

    Starting Railway Deployment. Created files Profile,runtime

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn 'passCBAP.wsgi'
--- passCBAP/settings.py
+++ passCBAP/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9#=)$rv00n#y1ppn0z)p3_n0+y65q3ii2sw-y3sbr3m$z&)_yf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
